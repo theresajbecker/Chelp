a04300010edab8bcbd272137a6074c525b59c2a5
Erik Kierstead <ErikKierstead@gmail.com>
2014-04-09 18:02:05 -0300

    Initial Rails Commit

---
+++ Code/Rails/Chelp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chelp::Application.config.secret_key_base = '730f2a51c1727d2b615495aaa08bf4b0cc3dcb248e1f478e7a19ef22c4337439c13a74c06e9e9eb00d195cba00a271920d33194d8c558211625beebeb124a485'
