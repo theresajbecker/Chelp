9b20c9b5061f6e4ba7ac24655819b1827221a253
Theresa Becker <theresa.j.becker@gmail.com>
2014-04-13 15:38:28 -0600

    charity review steps

---
+++ Code/Rails/features/steps_definitions/charity_review.rb
@@ -0,0 +1,46 @@
+#You can implement step definitions for undefined steps with these snippets:
+
+#When(/^I follow “User” that created the review$/) do
+#  pending # express the regexp above with the code you wish you had
+#end
+
+When(/^I follow “name”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to the “Users profile “$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow “Specific Charity”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to the “Charities Homepage”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow “Create User”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to the “Sign Up” page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow “Create Review”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to the “Create Review” page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow “Home”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to the “Chelp Homepage”$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
