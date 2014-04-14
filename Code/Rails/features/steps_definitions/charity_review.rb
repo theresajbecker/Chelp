c0cf82e10d1498ac97de9adef822762aedbd069d
Theresa Becker <theresa.j.becker@gmail.com>
2014-04-13 19:01:53 -0600

    apparently i hadn't committed my charity review step def?

--- Code/Rails/features/steps_definitions/charity_review.rb
+++ Code/Rails/features/steps_definitions/charity_review.rb
@@ -4,7 +4,7 @@
 #  pending # express the regexp above with the code you wish you had
 #end
 
-When(/^I follow “name”$/) do
+When(/^I follow “user”$/) do
   pending # express the regexp above with the code you wish you had
 end
 
@@ -12,35 +12,5 @@ Then(/^I should be taken to the “Users profile “$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-When(/^I follow “Specific Charity”$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
-Then(/^I should be taken to the “Charities Homepage”$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I follow “Create User”$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should be taken to the “Sign Up” page$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I follow “Create Review”$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should be taken to the “Create Review” page$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I follow “Home”$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should be taken to the “Chelp Homepage”$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
