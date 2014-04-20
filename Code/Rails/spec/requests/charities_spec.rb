30fa1f275b890c20835f64d2bb3503471dd42d20
Theresa Becker <theresa.j.becker@gmail.com>
2014-04-20 14:29:13 -0600

    no message

--- Code/Rails/spec/requests/charities_spec.rb
+++ Code/Rails/spec/requests/charities_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe "Charities" do
   describe "GET /charities" do
-    it "works! (now write some real specs)" do
+    it "Should have the content 'All Charities'" do
+    	visit '/charities'
+    	expect(page).to have_content('All Charities')
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get charities_path
-      response.status.should be(200)
+      #get charities_path
+      #response.status.should be(200)
     end
   end
 end
