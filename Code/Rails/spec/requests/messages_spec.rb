d16c8c95a56760756e8bf6a4c5090d668f9b24fd
Erik Kierstead <ErikKierstead@gmail.com>
2014-04-23 01:35:26 -0300

    Added Super DRY Code to messages_spec.rb to Test Edit/Back Links

--- Code/Rails/spec/requests/messages_spec.rb
+++ Code/Rails/spec/requests/messages_spec.rb
@@ -50,13 +50,49 @@ describe "Messages" do
       expect(page).to have_button("Submit")
       click_button "Submit"
       expect(page).to have_content('Message was successfully created.')
-    end 
+    end
+      
+  end   
+      
+  describe "GET /messages/1/edit" do
+  
+    before(:each) do
+      visit '/messages/new'
+      @firstname = "test first name"
+      @lastname = "test last name"
+      @username = "test username"
+      @email = "test email@email.com"
+      @message = "test Message"
+      fill_in "First Name", :with => @firstname
+      fill_in "Last Name", :with => @lastname
+      fill_in "Username", :with => @username
+      fill_in "Email", :with => @email
+      fill_in "Message", :with => @message
+      click_button "Submit"
+    end
 
-    it "should let me go back to message page" do
-      expect(page).to have_link("Back")
+    it "should let me return to main page" do
+      expect(page).to have_link("Back", :href=>"/messages")
       click_link("Back")
       expect(page).to have_content("Listing messages")
     end
+
+    it "should let me edit the message" do
+      expect(page).to have_link("Edit")
+      click_link "Edit"
+      expect(page).to have_content('Editing message')
+    end
+
+  end
+
+end
+        
+#          expect(page).to have_link("Back", :href=>"/messages")
+#          click_link("Back")
+#          expect(page).to have_content("Listing messages")
+        
+ #     end
+      
       
     #it "should let me edit the message" do
       #expect(page).to have_link("Edit")
@@ -64,6 +100,4 @@ describe "Messages" do
       #expect(page).to have_content('Editing message')
     #end
       
-  end
   
-end
