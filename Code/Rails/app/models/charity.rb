6285492356202d95152b679398db842ed4793f1a
Danielle <danielle.silverston@colorado.edu>
2014-05-03 01:35:46 -0300

    charity spec working

--- Code/Rails/app/models/charity.rb
+++ Code/Rails/app/models/charity.rb
@@ -1,3 +1,15 @@
 class Charity < ActiveRecord::Base
-	attr_accessible :name, :description, :filter_flags, :geographic_region, :area_of_impact, :percent_of_overhead, :religious_affiliation, :how_to_donate, :charity_homepage, :charity_image
+include ActiveModel::Validations
+	attr_accessible :name, 
+					:description, 
+					:filter_flags, 
+					:geographic_region, 
+					:area_of_impact, 
+					:percent_of_overhead, 
+					:religious_affiliation, 
+					:how_to_donate, 
+					:charity_homepage
+										
+
 end
+
--- Code/Rails/spec/models/charity_spec.rb
+++ Code/Rails/spec/models/charity_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Charity do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+  let(:charity) { 
+    charity = Charity.new(:name => 'test', 
+                    :description  => 'test',
+					:filter_flags  => 'test', 
+					:geographic_region  => 'test', 
+					:area_of_impact  => 'test', 
+					:percent_of_overhead  => 'test', 
+					:religious_affiliation  => 'test', 
+					:how_to_donate  => 'test', 
+					:charity_homepage  => 'test', 
+					:charity_image  => 'test',
+                    )}
+
+describe "attributes" do                    
+                    
+#it { should_not allow_mass_assignment_of(:password) } 
+it { should allow_mass_assignment_of(:name) }
+it { should allow_mass_assignment_of(:description) }
+it { should allow_mass_assignment_of(:filter_flags) }
+it { should allow_mass_assignment_of(:geographic_region) }
+it { should allow_mass_assignment_of(:area_of_impact) }
+it { should allow_mass_assignment_of(:percent_of_overhead) }
+it { should allow_mass_assignment_of(:religious_affiliation) }
+it { should allow_mass_assignment_of(:how_to_donate) }
+it { should allow_mass_assignment_of(:charity_homepage) }
+
+
+   end
+end
 end
