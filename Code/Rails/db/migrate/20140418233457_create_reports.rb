89171a6090d08190425377498c0e80ccc3736d8a
Alec Thilenius <Alec@Thilenius.com>
2014-05-04 21:06:29 -0600

    Switched large text fields over from string to text

--- Code/Rails/db/migrate/20140418233457_create_reports.rb
+++ Code/Rails/db/migrate/20140418233457_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
       t.references :user, index: true
-      t.string :message
+      t.text :message
 
       t.timestamps
     end
